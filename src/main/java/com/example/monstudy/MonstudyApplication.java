84604c7646d9a815e5c7de5ceaabfef9ebc46fe3
Lokie89 <traeuman@gmail.com>
2021-01-11 20:31:24 +0900

    init

---
+++ src/main/java/com/example/monstudy/MonstudyApplication.java
@@ -0,0 +1,13 @@
+package com.example.monstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+// https://brunch.co.kr/@springboot/530
+@SpringBootApplication
+public class MonstudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MonstudyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/monstudy/MonstudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.monstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonstudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
